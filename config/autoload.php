ecf7111f88d022281bd486a6518e081c1e80a65a
argiraud <a.giraud43@outlook.fr>
2017-12-20 12:08:12 +0100

    qqq

--- config/autoload.php
+++ config/autoload.php
@@ -1,13 +1,17 @@
 <?php
-class autoload
+class Autoload
 {
 	private static $_instance= null;
-	public static function charger(){
+
+    /**
+     *
+     */
+    public static function charger(){
 		if(null!=self::$_instance){
 			throw new RuntimeException(sprintf('%s is already started',__CLASS__));}
 		self::$_instance = new self();
-		if(!spl_autoload_register(array(self::$_instance,'_autoload'),false)){
-			throw new RuntimeException(sprintf('%s : Could not start the autoload',__CLASS__));}
+		if(!spl_autoload_register(array(self::$_instance,'autoload'),false)){
+			throw new RuntimeException(sprintf('%s : Could not start the autload',__CLASS__));}
 	}
 	private static function _autoload($class){
 		global $rep;
